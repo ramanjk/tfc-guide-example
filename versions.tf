ee7b56565be452acf56e6f0612d9c56c33bab5c3
ramanjk <30591604+ramanjk@users.noreply.github.com>
2021-03-12 12:51:14 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,13 +3,6 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
+    }  
   }
-
-  required_version = ">= 0.14.0"
 }
